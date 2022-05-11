547f4d90371ac152bf0a6333ed707bac44b286e1
vinu-2001 <105192760+vinu-2001@users.noreply.github.com>
2022-05-11 12:22:58 +0530

    Add files via upload

---
+++ hands-on/TableOf2.java
@@ -0,0 +1,14 @@
+package corejava;
+
+public class TableOf2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=10;i++)
+	    {
+	        System.out.println("2 * "+i+" = "+2*i);
+	    }
+
+	}
+
+}
