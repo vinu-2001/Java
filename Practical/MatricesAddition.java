4ad910d555d2f525490b558c0239e074983f2214
vinu-2001 <105192760+vinu-2001@users.noreply.github.com>
2022-05-23 11:47:25 +0530

    Add files via upload

---
+++ Practical/MatricesAddition.java
@@ -0,0 +1,56 @@
+package corejava;
+import java.util.Scanner;
+ 
+public class MatricesAddition {
+    public static void main(String[] args) {
+        
+        //Initializing array
+          int i, j;
+          int[][] a = new int[3][3];
+          int[][] b = new int[3][3];
+          int[][] c = new int[3][3];
+          
+          //creating scanner class object
+          Scanner s = new Scanner(System.in);
+          
+          //input for 1st matrix
+          System.out.print("Enter 9 elements for first matrix: ");
+          for(i=0; i<3; i++)
+          {
+             for(j=0; j<3; j++)
+             {
+                a[i][j] = s.nextInt();
+             }
+          }
+          
+          //input for 2nd matrix
+          System.out.print("Enter 9 elements for second matrix: ");
+          for(i=0; i<3; i++)
+          {
+             for(j=0; j<3; j++)
+             {
+                b[i][j] = s.nextInt();
+             }
+          }
+          
+          for(i=0; i<3; i++)
+          {
+             for(j=0; j<3; j++)
+             {
+                c[i][j] = a[i][j] + b[i][j];
+             }
+          }
+          
+          //Addition result
+          System.out.println("\n----Addition Result----");
+          for(i=0; i<3; i++)
+          {
+             for(j=0; j<3; j++)
+             {
+                System.out.print(c[i][j]+ " ");
+             }
+             System.out.print("\n");
+          }
+       }
+    }
+
