01266bec22e8f7d9efaa42ed43ca76d4e2317802
vinu-2001 <105192760+vinu-2001@users.noreply.github.com>
2022-05-24 13:44:23 +0530

    Add files via upload

---
+++ Practical/arrayList.java
@@ -0,0 +1,21 @@
+
+package corejava;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+//import java.util.*;
+
+public class arrayList {
+	public static void main(String args[]){  
+		ArrayList<String> list=new ArrayList<String>();//Creating arraylist  
+		list.add("Ravi");//Adding object in arraylist  
+		list.add("Vijay");  
+		list.add("Ravi");  
+		list.add("Ajay");  
+		//Traversing list through Iterator  
+		Iterator<String> itr=list.iterator();  
+		while(itr.hasNext()){  
+		System.out.println(itr.next());  
+		}  
+		}  
+}
