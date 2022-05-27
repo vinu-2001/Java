a6f5c50791e457a8b299d12c129e7be1489e938c
vinu-2001 <105192760+vinu-2001@users.noreply.github.com>
2022-05-27 23:40:13 +0530

    Add files via upload

---
+++ Practical/one.java
@@ -0,0 +1,34 @@
+package corejava;
+//Java program to illustrate the
+//concept of Multilevel inheritance
+
+
+class one {
+ public void print_geek()
+ {
+     System.out.println("Geeks");
+ }
+}
+
+class two extends one {
+ public void print_for() { System.out.println("for"); }
+}
+
+
+class three extends two {
+ public void print_geek()
+ {
+     System.out.println("Geeks");
+ }
+}
+
+//Derived class
+class MainOne {
+ public static void main(String[] args)
+ {
+     three g = new three();
+     g.print_geek();
+     g.print_for();
+     g.print_geek();
+ }
+}
