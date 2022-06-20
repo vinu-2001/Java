9f7841a93438eafc3abd39ead4a2fc71c2091dc3
vinu-2001 <105192760+vinu-2001@users.noreply.github.com>
2022-06-20 23:07:41 +0530

    Add files via upload

---
+++ Practical/Projects/HIbernateManyToMany/src/main/java/com/Hibernate/manytomany/Test/Test.java
@@ -0,0 +1,33 @@
+package com.Hibernate.manytomany.Test;
+
+import org.hibernate.Session;
+
+import com.Hibernate.manytomany.Entity.Employee;
+import com.Hibernate.manytomany.Entity.Project;
+import com.Hibernate.manytomany.Util.HibernateUtil;
+
+public class Test {
+    public static void main(String[] args) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+        // Create an employee
+        Employee employee = new Employee();
+        employee.setFirstName("Ramesh");
+        employee.setLastName("Fadatare");
+       // Create project1
+        Project project = new Project();
+        project.setTitle("Employee Management System");
+        // Create project2
+        Project project1 = new Project();
+       project1.setTitle("Content Management System");
+       // employee can work on two projects, Add project references in the employee
+       employee.getProjects().add(project);
+       employee.getProjects().add(project1);
+        // Add employee reference in the projects
+       project.getEmployees().add(employee);
+       project1.getEmployees().add(employee);
+       session.save(employee);
+       session.getTransaction().commit();
+       HibernateUtil.shutdown();
+    }
+}
